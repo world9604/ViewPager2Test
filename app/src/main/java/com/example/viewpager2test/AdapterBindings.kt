7874b662847752218e91d6db848702e1576644d9
gooditl <34080506+gooditl@users.noreply.github.com>
2021-01-07 13:37:00 +0900

    [refactor] Item.imgKey 리소스 수정
    - Item.imgKey 리소스 수정

--- app/src/main/java/com/example/viewpager2test/AdapterBindings.kt
+++ app/src/main/java/com/example/viewpager2test/AdapterBindings.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 object AdapterBindings {
 
@@ -22,14 +23,13 @@ object AdapterBindings {
 
     @BindingAdapter("bind:src")
     @JvmStatic
-    fun bindSrc(imageView: AppCompatImageView, imagePath: String?) {
-        Log.d(TEST, "bind:src : " + imagePath)
-        /*val path = KimpusConfig.getFileServer() + imagePath
+    fun bindSrc(imageView: AppCompatImageView, imgKey: String?) {
+        Log.d(TEST, "bind:src : " + imgKey)
         Picasso.get()
-                .load(path)
-                .placeholder(R.drawable.campustalk_logo)
-                .error(R.drawable.campustalk_logo)
+                .load(imgKey)
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
                 .fit()
-                .into(imageView)*/
+                .into(imageView)
     }
 }
--- app/src/main/java/com/example/viewpager2test/item.kt
+++ app/src/main/java/com/example/viewpager2test/item.kt
@@ -3,5 +3,5 @@ package com.example.viewpager2test
 data class Item (
     val title: String = "sample title",
     val content: String = "sample content",
-    val imgKey: String = "sample imgKey"
+    val imgKey: String = "https://d1d9isgar8w0es.cloudfront.net/development/campus_talk/group/thumbnail_img/UZE2KZGEFW53I7JVU4L0.jpeg"
 )
